ehsan18t/MagicX-Toolbox

commit 131c591859d760c8675e05d779d36772a8a7e007

[IMPROVED] Auto Update Checking

Toolbox.bat
@@ -10,7 +10,9 @@ SET "DESKTOP=%UserProfile%\Desktop"
 SET "AU_Temp_Path=%TEMP%\MagicXToolbox_psbdgtx"
 SET "Update_Path=%Current_Dir%\Update"
 
-CALL :Check_AU >NUL 2>&1
+IF NOT EXIST "%AU_Temp_Path%" (
+    CALL :Check_AU >NUL 2>&1
+)
 
 FLTMC >NUL 2>&1 || (
 	ECHO SET UAC = CreateObject^("Shell.Application"^) > "%TEMP%\GetAdmin.vbs"

commit d0ec1ddd4afc7c7a8b9748f35b60579e01c4aeb2

[FIXED] Potential bug in Auto update checking (introduced in 131c591859d760c8675e05d779d36772a8a7e007)

Toolbox.bat
@@ -10,7 +10,7 @@ SET "DESKTOP=%UserProfile%\Desktop"
 SET "AU_Temp_Path=%TEMP%\MagicXToolbox_psbdgtx"
 SET "Update_Path=%Current_Dir%\Update"
 
-IF NOT EXIST "%AU_Temp_Path%" (
+IF NOT EXIST "%AU_Temp_Path%\UpdateAvailable.yes" (
     CALL :Check_AU >NUL 2>&1
 )
 